b72bd4fdbca565d87786a84ccc9bfe79c92f5a4a
Benjamin Turner <ben@moonbase.ai>
2026-02-19 15:17:14 -0800

    fix: add missing imports for nested recursive filters
    These filters are reference by ItemsFilterParam, but are Forward References there to avoid problems with circular imports. Those nested recursive filter types need to be imported into the namespace of ItemSearchParams in order for them to be resolved correctly by Pydantic v1. Without them, the pydantic test pass reports a bunch of errors like this when trying to use the collection item search method:
    `NameError: name 'ItemsFilterAndGroupParam' is not defined`

--- src/moonbase/types/collections/item_search_params.py
+++ src/moonbase/types/collections/item_search_params.py
@@ -52,3 +52,6 @@ class ItemSearchParams(TypedDict, total=False):
 
 
 from ..items_filter_param import ItemsFilterParam
+from ..items_filter_or_group_param import ItemsFilterOrGroupParam
+from ..items_filter_and_group_param import ItemsFilterAndGroupParam
+from ..items_filter_not_group_param import ItemsFilterNotGroupParam
